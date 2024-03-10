4ecaab01d7c960a0b6c2d2cd22c9cb895c5b3853
Joselu <joseluissgz97@gmail.com>
2024-03-10 13:45:09 +0100

    Icons for isActive

--- src/components/is-active/IsActive.jsx
+++ src/components/is-active/IsActive.jsx
@@ -1,9 +1,18 @@
+import CheckCircleIcon from '../icons/CheckCircleIcon';
+import CrossCircleIcon from '../icons/CrossCircleICon';
 import style from './IsActive.module.css';
 
 const IsActive = ({ isActive }) => {
 	const activeClass = isActive ? style.user__active : style.user__inactive;
 
-	return <span className={activeClass}></span>;
+	const Icon = isActive ? CheckCircleIcon : CrossCircleIcon;
+
+	return (
+		<span className={activeClass}>
+			<Icon className={style.user__icon} />
+			{isActive ? 'Activo' : 'Inactivo'}
+		</span>
+	);
 };
 
 export default IsActive;
