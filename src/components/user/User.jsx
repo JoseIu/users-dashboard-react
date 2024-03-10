14d50c5ffe223ed386d018b37afc31ee4924a424
Joselu <joseluissgz97@gmail.com>
2024-03-10 13:46:05 +0100

    new desing for user, card to row (list)

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -2,21 +2,16 @@ import IsActive from '../is-active/IsActive';
 import UserRole from '../user-role/UserRole';
 import style from './User.module.css';
 
-const User = ({ id, name, active, role, img, toggleUserActive }) => {
+const User = ({ name, username, active, role, img }) => {
 	return (
 		<article className={style.user}>
-			<img className={style.user__img} src={img} alt={name} />
-			<div className={style.user__info}>
-				<h2 className={style.user__name}>
-					{name}
-					<UserRole role={role} />
-				</h2>
-
-				<IsActive isActive={active} />
+			<div className=''>
+				<img className={style.user__img} src={img} alt={name} />
+				<span className={style.user__username}>{username} </span>
 			</div>
-			<button onClick={() => toggleUserActive(id)} className={style.user__btn}>
-				{active ? 'Disable' : 'Activate'}
-			</button>
+			<h2 className={style.user__name}>{name}</h2>
+			<IsActive isActive={active} />
+			<UserRole role={role} />
 		</article>
 	);
 };
