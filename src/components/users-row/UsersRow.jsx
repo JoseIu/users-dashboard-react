e103bf3e12af7a11a495cad0a6a606d1a3e88b4e
Joselu <joseluissgz97@gmail.com>
2024-03-10 13:47:25 +0100

    removing toogleUserActive

--- src/components/users-row/UsersRow.jsx
+++ src/components/users-row/UsersRow.jsx
@@ -1,12 +1,10 @@
 import User from '../user/User';
 import style from './UsersRow.module.css';
 
-const UsersRow = ({ users, toggleUserActive }) => {
+const UsersRow = ({ users }) => {
 	if (!users.length) return 'Usuario no encontrado 😅';
 
-	const usersRendered = users.map((user) => (
-		<User {...user} toggleUserActive={toggleUserActive} key={user.id} />
-	));
+	const usersRendered = users.map((user) => <User {...user} key={user.id} />);
 
 	return <div className={style.users}>{usersRendered}</div>;
 };
