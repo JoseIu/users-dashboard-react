d98ebe74bf30bd5e439919f50e0254a068dfd381
Joselu <joseluissgz97@gmail.com>
2024-03-11 17:21:30 +0100

    importing userFilters and creating usersPagination

--- src/components/user-list/UserList.jsx
+++ src/components/user-list/UserList.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
+import { SORT_OPTIONS } from '../../constants/sortOptions';
+import {
+	filterByName,
+	filterOnlyActive,
+	sortBy,
+} from '../../lib/users/userFilters';
 import Title from '../title/Tile';
 import UserFilters from '../user-filters/UserFilters';
+import UserListPagination from '../user-list-pagination/UserListPagination';
 import UsersRow from '../users-row/UsersRow';
 import style from './UserList.module.css';
 
@@ -8,11 +15,17 @@ const UserList = ({ initialUsers }) => {
 	const { search, setSearch, onlyActive, setOnlyActive, sort, setSort } =
 		useFilters();
 
-	const [users, setUsers] = useState(initialUsers);
+	const [page, setPages] = useState(1);
+	const [itemsPerPage, setItemsPerPage] = useState(3);
 
-	let usersFiltered = filterOnlyActive(users, onlyActive);
-	usersFiltered = filterByName(usersFiltered, search);
-	usersFiltered = sortBy(usersFiltered, sort);
+	const { usersFiltered } = getUsers(
+		initialUsers,
+		onlyActive,
+		search,
+		sort,
+		page,
+		itemsPerPage,
+	);
 
 	return (
 		<div className={style.container}>
@@ -26,8 +39,37 @@ const UserList = ({ initialUsers }) => {
 				setSort={setSort}
 			/>
 			<UsersRow users={usersFiltered} />
+			<UserListPagination
+				page={page}
+				setPage={setPages}
+				itemPerPage={itemsPerPage}
+				setItemsPerPage={setItemsPerPage}
+			/>
 		</div>
 	);
+};
+
+const usersPagination = (users, page, itemsPerPage) => {
+	const startIndex = (page - 1) * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+
+	return users.slice(startIndex, endIndex);
+};
+const getUsers = (
+	initialUsers,
+	onlyActive,
+	search,
+	sort,
+	page,
+	itemsPerPage,
+) => {
+	let usersFiltered = filterOnlyActive(initialUsers, onlyActive);
+	usersFiltered = filterByName(usersFiltered, search);
+	usersFiltered = sortBy(usersFiltered, sort);
+
+	usersFiltered = usersPagination(usersFiltered, page, itemsPerPage);
+
+	return { usersFiltered };
 };
 
 const useFilters = () => {
@@ -39,59 +81,14 @@ const useFilters = () => {
 
 	const setSearch = (search) => setFilters({ ...filters, search });
 	const setOnlyActive = (onlyActive) => {
-		if (onlyActive && filters.sort === 3) {
-			return setFilters({ ...filters, sort: 0, onlyActive });
+		if (onlyActive && filters.sort === SORT_OPTIONS.ACTIVE) {
+			return setFilters({ ...filters, sort: SORT_OPTIONS.DEFAULT, onlyActive });
 		}
 		setFilters({ ...filters, onlyActive });
 	};
 	const setSort = (sort) => setFilters({ ...filters, sort });
 
 	return { ...filters, setSearch, setOnlyActive, setSort };
-};
-
-const filterByName = (users, search) => {
-	if (!search) return users;
-	const normalize = search.toLowerCase();
-	const filteredUsers = users.filter((user) =>
-		user.name.toLowerCase().includes(normalize),
-	);
-	return filteredUsers;
-};
-
-const filterOnlyActive = (users, active) => {
-	if (active === false) return users;
-
-	const filteredUsers = users.filter((user) => user.active);
-
-	return filteredUsers;
-};
-
-const sortBy = (users, sort) => {
-	const usersToSort = [...users];
-	switch (sort) {
-		case 1:
-			return usersToSort.sort((a, b) => {
-				if (a.name > b.name) return 1;
-				if (a.name < b.name) return -1;
-				return 0;
-			});
-		case 2:
-			return usersToSort.sort((a, b) => {
-				if (a.role === b.role) return 0;
-				if (a.role === 'profesor') return -1;
-				if (a.role === 'estudiante' && b.role === 'otro') return -1;
-
-				return 1;
-			});
-		case 3:
-			return usersToSort.sort((a, b) => {
-				if (a.active === b.active) return 0;
-				if (a.active && !b.active) return -1;
-				return 1;
-			});
-		default:
-			return users;
-	}
 };
 
 export default UserList;
