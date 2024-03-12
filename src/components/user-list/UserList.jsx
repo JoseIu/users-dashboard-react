cf4d219ae84843ce3b459d19a8f0bf0b970ca8d8
Joselu <joseluissgz97@gmail.com>
2024-03-12 09:22:16 +0100

    refactoring and adding fetch

--- src/components/user-list/UserList.jsx
+++ src/components/user-list/UserList.jsx
@@ -1,31 +1,29 @@
-import { useState } from 'react';
-import { SORT_OPTIONS } from '../../constants/sortOptions';
+import { useEffect, useState } from 'react';
+import useFilters from '../../lib/hooks/useFilters';
 import {
 	filterByName,
 	filterOnlyActive,
 	sortBy,
 } from '../../lib/users/userFilters';
+import usersPagination from '../../lib/users/usersPagination';
 import Title from '../title/Tile';
 import UserFilters from '../user-filters/UserFilters';
 import UserListPagination from '../user-list-pagination/UserListPagination';
 import UsersRow from '../users-row/UsersRow';
 import style from './UserList.module.css';
 
-const UserList = ({ initialUsers }) => {
+const UserList = () => {
+	// filters is a objet with all states (searh,onlyActive,page,totalpage...)
 	const {
 		filters,
 		setSearch,
-
 		setOnlyActive,
-
 		setSort,
-
 		setPages,
-
 		setItemsPerPage,
 	} = useFilters();
 
-	const { usersFiltered, totalPage } = getUsers(initialUsers, filters);
+	const { usersPaginated, totalPage } = useUsers(filters);
 
 	return (
 		<div className={style.container}>
@@ -38,72 +36,52 @@ const UserList = ({ initialUsers }) => {
 				sort={filters.sort}
 				setSort={setSort}
 			/>
-			<UsersRow users={usersFiltered} />
+			<UsersRow users={usersPaginated} />
 			<UserListPagination
 				page={filters.page}
 				setPage={setPages}
-				itemPerPage={filters.itemsPerPage}
+				itemsPerPage={filters.itemsPerPage}
 				setItemsPerPage={setItemsPerPage}
 				totalPage={totalPage}
 			/>
 		</div>
 	);
 };
+const fetchUsers = async (setUsers, signal) => {
+	const response = await fetch('http://localhost:3000/users', { signal });
 
-const usersPagination = (users, page, itemsPerPage) => {
-	const startIndex = (page - 1) * itemsPerPage;
-	const endIndex = startIndex + itemsPerPage;
-
-	return users.slice(startIndex, endIndex);
+	const data = await response.json();
+	setUsers(data);
 };
-const getUsers = (
-	initialUsers,
+
+const usersToDisplay = (
+	users,
 	{ onlyActive, search, sort, page, itemsPerPage },
 ) => {
-	let usersFiltered = filterOnlyActive(initialUsers, onlyActive);
+	let usersFiltered = filterOnlyActive(users, onlyActive);
 	usersFiltered = filterByName(usersFiltered, search);
 	usersFiltered = sortBy(usersFiltered, sort);
-	const totalPage = Math.ceil(usersFiltered.length / itemsPerPage);
 
-	usersFiltered = usersPagination(usersFiltered, page, itemsPerPage);
+	const { totalPage, usersPaginated } = usersPagination(
+		usersFiltered,
+		page,
+		itemsPerPage,
+	);
 
-	return { usersFiltered, totalPage };
+	return { usersPaginated, totalPage };
 };
+const useUsers = (filters) => {
+	const [users, setUsers] = useState([]);
 
-const useFilters = () => {
-	const [filters, setFilters] = useState({
-		search: '',
-		onlyActive: false,
-		sort: 0,
-		page: 1,
-		itemsPerPage: 3,
-	});
+	useEffect(() => {
+		const controller = new AbortController();
+		fetchUsers(setUsers, controller.signal);
 
-	const setSearch = (search) => setFilters({ ...filters, page: 1, search });
-	const setOnlyActive = (onlyActive) => {
-		if (onlyActive && filters.sort === SORT_OPTIONS.ACTIVE) {
-			return setFilters({
-				...filters,
-				sort: SORT_OPTIONS.DEFAULT,
-				page: 1,
-				onlyActive,
-			});
-		}
-		setFilters({ ...filters, onlyActive });
-	};
-	const setSort = (sort) => setFilters({ ...filters, sort });
+		return () => controller.abort();
+	}, []);
+	const { usersPaginated, totalPage } = usersToDisplay(users, filters);
 
-	const setPages = (page) => setFilters({ ...filters, page });
-	const setItemsPerPage = (itemsPerPage) => ({ ...filters, itemsPerPage });
-
-	return {
-		filters,
-		setSearch,
-		setOnlyActive,
-		setSort,
-		setPages,
-		setItemsPerPage,
-	};
+	return { usersPaginated, totalPage };
 };
 
 export default UserList;
