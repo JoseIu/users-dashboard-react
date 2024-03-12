8d26528983383fe970e57eedd7b4a467f505b481
Joselu <joseluissgz97@gmail.com>
2024-03-12 09:21:45 +0100

    changing options values

--- src/components/user-list-pagination/UserListPagination.jsx
+++ src/components/user-list-pagination/UserListPagination.jsx
@@ -5,25 +5,27 @@ import style from './UserListPagination.module.css';
 const UserListPagination = ({
 	page,
 	setPage,
-	itemPerPage,
+	itemsPerPage,
 	setItemsPerPage,
 	totalPage,
-}) => (
-	<div className={style.pagination}>
-		<div className={style.pagination__select}>
-			<Select
-				value={itemPerPage}
-				onChange={(ev) => setItemsPerPage(Number(ev.target.value))}
-			>
-				<option value={3}>3</option>
-				<option value={4}>4</option>
-				<option value={6}>6</option>
-			</Select>
-			<span>Elementos por página</span>
-		</div>
+}) => {
+	return (
+		<div className={style.pagination}>
+			<div className={style.pagination__select}>
+				<Select
+					value={itemsPerPage}
+					onChange={(ev) => setItemsPerPage(Number(ev.target.value))}
+				>
+					<option value={4}>4</option>
+					<option value={6}>6</option>
+					<option value={8}>8</option>
+				</Select>
+				<span>Elementos por página</span>
+			</div>
 
-		<PageSelector page={page} setPage={setPage} totalPage={totalPage} />
-	</div>
-);
+			<PageSelector page={page} setPage={setPage} totalPage={totalPage} />
+		</div>
+	);
+};
 
 export default UserListPagination;
